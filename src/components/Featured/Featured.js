1532fb3a02046b33a6ecddfa7df559aed542729e
Andrew Hill <contact@andrewthomashill.co.uk>
2023-05-03 09:03:37 +0200

    Migrate styling to PCSS

---
+++ src/components/Featured/Featured.js
@@ -0,0 +1,29 @@
+import styles from './Featured.module.css';
+import React from 'react';
+import Details from '../Details/Details';
+import PostImage from '../PostImage/PostImage';
+
+const Featured = ({ className, hasAuthor, post, tag: Tag }) => {
+  return (
+    <Tag className={`${styles.featured} ${className}`}>
+      <Details className={styles.details} hasAuthor={hasAuthor} heading="h2" post={post} />
+      <PostImage
+        alt={post.title}
+        className={styles.image}
+        isLazy={false}
+        image={post.image || post.old_image}
+        ratio={75}
+        sizes="(max-width: 768px) 100vw, (max-width: 1388px) 60vw, 870px"
+        srcset={[400, 800, 1600]}
+      />
+    </Tag>
+  );
+}
+
+Featured.defaultProps = {
+  className: '',
+  hasAuthor: true,
+  tag: 'article',
+}
+
+export default Featured;
